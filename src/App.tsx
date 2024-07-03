1af5fa29901420abcbd221262a2aa09f138da694
Chayanon Pitak <chayanon@ddxtransformation.com>
2024-07-03 14:47:43 +0700

    feat: todo

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,56 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { useState } from 'react';
 
-function App() {
+export default function App() {
+  const [list, setList] = useState<{taskName: string, completed: boolean}[]>([])
+  const [newTaskName, setNewTaskname] = useState<string>('New Task')
+
+  const taskList = list.map((l,i) => (
+    <ListItem
+      key={i}
+      taskName={l.taskName}
+      isCompleted={l.completed}
+      handleStatusChange={() => setList([...list.slice(0, i), {taskName: l.taskName, completed: !l.completed},...list.slice(i+1)])}
+      handleDelete={() => setList([...list.slice(0, i), ...list.slice(i+1)])}
+    />
+  ))
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+    <div>
+      <button 
+          type='button' 
+          onClick={() => {setList(list.filter((l) => !l.completed))}}
         >
-          Learn React
-        </a>
-      </header>
+          Delete Completed
+        </button>
+      {taskList}
+      <div style={{display: 'flex'}}>
+        <button 
+          type='button' 
+          onClick={() => {setList([...list, {taskName: newTaskName, completed: false}])}}
+        >
+          +
+        </button>
+        <input type='text' value={newTaskName} onChange={(e) => setNewTaskname(e.target.value)}/>
+      </div>
     </div>
   );
 }
 
-export default App;
+function ListItem({ 
+  taskName,
+  isCompleted,
+  handleStatusChange,
+  handleDelete,
+} : { 
+  taskName: string
+  isCompleted: boolean
+  handleStatusChange: () => void,
+  handleDelete: () => void
+}) {
+  return (
+    <div style={{display: 'flex'}}>
+      <button type='button' onClick={handleDelete}>X</button>
+      <input type='checkbox' checked={isCompleted} onChange={handleStatusChange}/>
+      <div style={{textDecoration: isCompleted ? 'line-through' : 'none'}}>{taskName}</div>
+    </div>
+  );
+}
